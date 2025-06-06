3a94b80466a2815112c71e02c6436cfd8bbf1776
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-06 12:02:16 +0900

    feat: CommunitySidebar 추가

---
+++ src/components/navigation/CommunitySidebar.jsx
@@ -0,0 +1,13 @@
+function CommunitySidebar() {
+  return (
+    <div className="h-full p-4 dark:text-white">
+      <ul className="space-y-3">
+        <li className="text-xl">오재현</li>
+        <li className="text-xl">서지민</li>
+        <li className="text-xl">맞아아니야</li>
+      </ul>
+    </div>
+  );
+}
+
+export default CommunitySidebar;
