227ba80d3fb20b05ac8a6bd5064b2cdf4d8c8004
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-06 12:04:30 +0900

    feat: 채팅 페이지 추가

---
+++ src/routes/chat/ChattingPage.jsx
@@ -0,0 +1,7 @@
+export default function ChattingPage() {
+  return (
+    <div className="min-h-screen bg-white dark:bg-zinc-800 text-black dark:text-white p-8">
+      <p className="text-2xl">채팅 페이지임다아아아</p>
+    </div>
+  );
+}
