e330deb36131fbdba02da3834ec91c2c00c8b614
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-06 12:02:57 +0900

    feat: 색상 테마 컨텍스트 추가

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+export default function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "basic";
+  });
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
