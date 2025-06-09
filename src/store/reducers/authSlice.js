534644b3a4312ecd5d3c88abd09fbbdf9612a11f
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-09 12:17:07 +0900

    feat: 미완성 슬라이스 추가

---
+++ src/store/reducers/authSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  id: null,
+  email: "",
+  nickname: "",
+  profileImage: "",
+  signupType: "",
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { id, email, nickname, profileImage, signupType } = action.payload;
+      state.id = id;
+      state.email = email;
+      state.nickname = nickname;
+      state.profileImage = profileImage;
+      state.signupType = signupType;
+      state.isLoggedIn = true;
+    },
+    logout: (state) => {
+      state.id = null;
+      state.email = "";
+      state.nickname = "";
+      state.profileImage = "";
+      state.signupType = "";
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const { setUser, logout } = authSlice.actions;
+export default authSlice.reducer;
