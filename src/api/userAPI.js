1c032f7b1b72b897f0e4048a7f52449dcb4bdf70
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-09 12:18:02 +0900

    feat: 유저 api 추가

---
+++ src/api/userAPI.js
@@ -0,0 +1,46 @@
+import BaseApi from "./axiosInstance";
+
+class UserApi extends BaseApi {
+  constructor() {
+    super();
+  }
+
+  async checkEmail(email) {
+    const res = await this.fetcher.get(`/user/check-email`, {
+      params: { email },
+    });
+    return res.data;
+  }
+
+  async join(data) {
+    const res = await this.fetcher.post("/user/join", data);
+    return res.data;
+  }
+
+  async login(data) {
+    const res = await this.fetcher.post("/user/login", data);
+    return res.data;
+  }
+
+  async getUserInfo(userId) {
+    const res = await this.fetcher.get(`/user/${userId}`);
+    return res.data;
+  }
+
+  async updateUserInfo(data) {
+    const res = await this.fetcher.put("/user", data);
+    return res.data;
+  }
+
+  async findEmail(data) {
+    const res = await this.fetcher.post("/user/find-email", data);
+    return res.data;
+  }
+
+  async findPassword(data) {
+    const res = await this.fetcher.post("/user/find-password", data);
+    return res.data;
+  }
+}
+
+export default new UserApi();
