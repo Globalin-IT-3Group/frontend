112356f1b08afb6f198c1128872f299551f6a5a6
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-09 15:57:30 +0900

    feat: user 테스트 api 적용

--- src/routes/main/MainPage.jsx
+++ src/routes/main/MainPage.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import UserApi from "../../api/userAPI";
 export default function MainPage() {
+  useEffect(() => {
+    const fetchTest = async () => {
+      try {
+        const result = await UserApi.test();
+        console.log("결과!!!!!: ", result);
+      } catch (error) {
+        console.error("으앙아ㅏ아악", error);
+      }
+    };
+
+    fetchTest();
+  }, []);
+
   return (
     <div className="min-h-screen bg-white dark:bg-zinc-800 text-black dark:text-white p-8 transition-all duration-300">
       <p className="text-2xl">메인 페이지입니당!</p>
