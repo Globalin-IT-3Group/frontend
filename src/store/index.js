24c79d664865c0a79f124027c3ecb31356e5fe81
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-09 14:05:33 +0900

    feat: 리덕스 persist 적용

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,45 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/authSlice";
 
-export default configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage 사용
+
+import { combineReducers } from "redux";
+
+// 1. 리듀서 합치기
+const rootReducer = combineReducers({
+  auth: authReducer,
 });
+
+// 2. persist 설정
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// 3. persist reducer 생성
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// 4. store 생성
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // redux-persist 관련 액션들 무시
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+// 5. persistor 생성
+export const persistor = persistStore(store);
