c0d2d204610d61d03719c671a8d308c15a412247
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-10 20:08:59 +0900

    feat: 텍스트 속성 추가 및 내용 변경

--- src/routes/chat/ChattingPage.jsx
+++ src/routes/chat/ChattingPage.jsx
@@ -10,8 +10,8 @@ export default function ChattingPage() {
   return roomId ? (
     <ChatRoom roomId={roomId} otherUser={otherUser} />
   ) : (
-    <div className="flex justify-center items-center w-full h-full text-gray-500">
-      채팅방을 선택해주세요.
+    <div className="pt-10 flex justify-center items-center w-full h-full text-gray-500">
+      <p className="text-bold">채팅방을 선택해주세요!</p>
     </div>
   );
 }
