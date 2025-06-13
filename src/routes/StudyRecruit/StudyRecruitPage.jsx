27bf689bfd32ebc9f34beed42d5c001bede24272
agreenroom <tjwlals7805@gmail.com>
2025-06-13 16:54:47 +0900

    hot fix

--- src/routes/StudyRecruit/StudyRecruitPage.jsx
+++ src/routes/StudyRecruit/StudyRecruitPage.jsx
@@ -2,9 +2,11 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import RecruitBoxContainer from "../../components/StudyRecruit/RecruitBoxContainer";
 import StudyRecruitAPI from "../../api/studyRecruit";
 import { useState, useEffect } from "react";
+import { HashtagIcon } from "@heroicons/react/24/outline";
 
 export default function StudyRecruitPage() {
   const [studyRoomList, setStudyRoomList] = useState([]);
+  const [showSearch, setShowSearch] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,37 +25,56 @@ export default function StudyRecruitPage() {
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mx-auto mt-14">스터디 구인</h1>
-      <div className="bg-white  w-full h-auto p-12 mt-11 flex flex-col overflow-hidden">
-        <div className="relative w-full max-w-[500px] mx-auto mb-6">
-          <input
-            type="text"
-            placeholder="스터디 그룹명을 검색하세요"
-            className="w-full h-[50px] rounded-2xl p-4 pr-12 border border-gray-300 shadow-[0_0_2px_rgba(0,0,0,0.1)]"
-          />
-
-          <button
-            className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 hover:text-black focus:outline-none active:scale-95 transition cursor-pointer"
-            onClick={() => console.log("검색")}
-          >
-            <MagnifyingGlassIcon className="w-5 h-5" />
-          </button>
-        </div>
-
-        <div className="relative h-full">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 h-full pr-2">
-            {studyRoomList.map((study) => (
-              <RecruitBoxContainer
-                key={study.id}
-                image="/6.jpg"
-                roomName={study.title}
-                studyExplain={study.studyExplain}
-                profileImage={study.leader.profileImage}
-                leader={study.leader.nickname}
-                createdAt={study.createdAt}
-                userCount={`${study.userCount}/4 모집 완료`}
-              />
-            ))}
+      <div className="bg-white w-full h-auto p-8 mt-10 flex flex-col overflow-hidden">
+        <div className="flex items-center justify-between">
+          <div className="flex space-x-8 ml-4">
+            <span className="text-md text-gray-600 hover:text-black cursor-pointer">
+              New
+            </span>
+            <span className="text-md text-gray-600 hover:text-black cursor-pointer">
+              Most View
+            </span>
+            <span className="text-md text-gray-600 hover:text-black cursor-pointer">
+              My Request
+            </span>
           </div>
+          <div className="flex items-center justify-end gap-4 w-full max-w-3xl relative">
+            <div className="relative h-[50px]">
+              <input
+                type="text"
+                placeholder="스터디 그룹명을 검색하세요"
+                className={`absolute right-0 h-[50px] rounded-2xl border border-gray-300 shadow pl-4 pr-4 bg-white
+          transition-all duration-300 ease-in-out
+          ${
+            showSearch
+              ? "w-[300px] opacity-100"
+              : "w-0 opacity-0 overflow-hidden"
+          }`}
+              />
+            </div>
+            <MagnifyingGlassIcon
+              className="w-5 h-5 text-gray-500 hover:text-black cursor-pointer"
+              onClick={() => setShowSearch((prev) => !prev)}
+            />
+            <HashtagIcon className="w-5 h-5 text-gray-500 hover:text-black cursor-pointer" />
+          </div>
+        </div>
+      </div>
+
+      <div className="relative h-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 h-full pr-2">
+          {studyRoomList.map((study) => (
+            <RecruitBoxContainer
+              key={study.id}
+              image="/6.jpg"
+              roomName={study.title}
+              studyExplain={study.studyExplain}
+              profileImage={study.leader.profileImage}
+              leader={study.leader.nickname}
+              createdAt={study.createdAt}
+              userCount={`${study.userCount}/4 모집 완료`}
+            />
+          ))}
         </div>
       </div>
     </div>
