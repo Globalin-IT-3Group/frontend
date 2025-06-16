ee52d9d7c1d5fb6155d82df56a961d5f4d6f812b
agreenroom <tjwlals7805@gmail.com>
2025-06-16 21:54:07 +0900

    feat:스토리 카드 모달 생성

---
+++ src/components/MyStudyRoom/StudyRoomListContainer.jsx
@@ -0,0 +1,9 @@
+export default function StudyRoomListContainer({ children }) {
+  return (
+    <div>
+      <div className="w-[350px] h-[500px] mx-auto p-6 bg-yellow-200 rounded-2xl">
+        {children}
+      </div>
+    </div>
+  );
+}
--- src/components/MyStudyRoom/StudyRoomListModal.jsx
+++ src/components/MyStudyRoom/StudyRoomListModal.jsx
@@ -1,66 +1,65 @@
-import { Link } from "react-router-dom";
-import Slider from "react-slick";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import StudyRoomListContainer from "./StudyRoomListContainer";
 
-export default function StudyRoomListModal({ onClose }) {
-  const settings = {
-    dots: true,
-    fade: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    waitForAnimate: false,
-  };
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
+export default function StudyRoomListModal() {
   return (
-    <div
-      className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center"
-      onClick={(e) => {
-        if (e.target === e.currentTarget) onClose();
-      }}
-    >
-      <div className="bg-white rounded-4xl w-[500px] h-[600px] p-6 shadow-md flex items-center justify-center">
-        <div className="slider-container w-full h-full">
-          <Slider {...settings}>
-            <div className="flex items-center justify-center w-full h-[500px]">
-              <Link to="/study/mystudyroom">
-                <img
-                  src="/pinga1.jpg"
-                  alt="study1"
-                  className="w-[400px] h-[400px] rounded-2xl object-cover mx-auto flex flex-col items-center mt-10"
-                />
-              </Link>
-            </div>
-            <div className="flex items-center justify-center w-full h-[500px]">
-              <Link to="/study/mystudyroom">
-                <img
-                  src="/pinga2.jpg"
-                  alt="study2"
-                  className="w-40 h-40 rounded-2xl object-cover"
-                />
-              </Link>
-            </div>
-            <div className="flex items-center justify-center w-full h-[500px]">
-              <Link to="/study/mystudyroom">
-                <img
-                  src="/pinga3.jpg"
-                  alt="study3"
-                  className="w-40 h-40 rounded-2xl object-cover"
-                />
-              </Link>
-            </div>
-            <div className="flex items-center justify-center w-full h-[500px]">
-              <Link to="/study/mystudyroom">
-                <img
-                  src="/pinga4.jpg"
-                  alt="study4"
-                  className="w-40 h-40 rounded-2xl object-cover"
-                />
-              </Link>
-            </div>
-          </Slider>
-        </div>
-      </div>
+    <div className="w-[900px] overflow-visible">
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={3}
+        spaceBetween={0}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: -30,
+          depth: 200,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        loop={false}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwipe"
+      >
+        <SwiperSlide>
+          <StudyRoomListContainer>
+            <img
+              src="https://swiperjs.com/demos/images/nature-1.jpg"
+              className="w-[300px] h-auto object-cover rounded-xl"
+            />
+          </StudyRoomListContainer>
+        </SwiperSlide>
+        <SwiperSlide>
+          <StudyRoomListContainer>
+            <img
+              src="https://swiperjs.com/demos/images/nature-2.jpg"
+              className="w-[300px] h-auto object-cover rounded-xl"
+            />
+          </StudyRoomListContainer>
+        </SwiperSlide>
+        <SwiperSlide>
+          <StudyRoomListContainer>
+            <img
+              src="https://swiperjs.com/demos/images/nature-3.jpg"
+              className="w-[300px] h-auto object-cover rounded-xl"
+            />
+          </StudyRoomListContainer>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://swiperjs.com/demos/images/nature-4.jpg"
+            className="w-[400px] h-auto object-cover rounded-xl"
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
