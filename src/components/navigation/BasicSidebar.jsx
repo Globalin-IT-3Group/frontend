2f7350cdd8ef12f87c12191df228d5b4784b5981
agreenroom <tjwlals7805@gmail.com>
2025-06-16 13:08:03 +0900

    feat: 사이드바 노트 아이콘 라우터 연결

--- src/components/navigation/BasicSidebar.jsx
+++ src/components/navigation/BasicSidebar.jsx
@@ -21,7 +21,7 @@ function BasicSidebar() {
           </Link>
         </li>
         <li className="flex flex-col items-center gap-3">
-          <Link to="/" className="flex flex-col items-center gap-3 ">
+          <Link to="/note" className="flex flex-col items-center gap-3 ">
             <label className="cursor-pointer inline-flex items-center justify-center bg-white rounded-full p-2 text-2xl shadow-[0_0_6px_rgba(0,0,0,0.1)] hover:text-white hover:bg-gradient-to-bl from-blue-200 to-blue-400 transition-all duration-300">
               📚
             </label>
