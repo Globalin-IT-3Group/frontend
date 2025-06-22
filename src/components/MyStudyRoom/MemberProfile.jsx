57c6d1b98d4378d181a81932b1eb1d90aae98b59
agreenroom <tjwlals7805@gmail.com>
2025-06-22 23:02:41 +0900

    feat: 스터디룸 멤버프로필 스켈레톤 생성

--- src/components/MyStudyRoom/MemberProfile.jsx
+++ src/components/MyStudyRoom/MemberProfile.jsx
@@ -11,9 +11,9 @@ export default function MemberProfile({
   console.log(studyRecruit);
 
   return (
-    <div className="flex flex-col gap-4 relative w-fit">
+    <div className="flex w-full flex-col gap-4 relative w-fit">
       {/* 방장 */}
-      <div className="flex w-[450px] h-[120px] bg-blue-100 rounded-2xl shadow-[0_0_6px_rgba(0,0,0,0.1)]">
+      <div className="flex h-[120px] bg-blue-100 rounded-2xl shadow-[0_0_6px_rgba(0,0,0,0.1)]">
         {/* 프로필+별 겹침 레이아웃 */}
         <div className="w-1/2 flex items-center justify-center p-4 gap-4">
           <div className="relative">
@@ -27,7 +27,9 @@ export default function MemberProfile({
               className="w-18 h-18 rounded-full shadow-[0_0_6px_rgba(0,0,0,0.1)] object-cover"
             />
           </div>
-          <p className="font-bold text-lg">{leader?.nickname || "방장"}</p>
+          <p className="font-bold text-lg whitespace-nowrap">
+            {leader?.nickname || "방장"}
+          </p>
         </div>
         {/* 구인 작성 버튼 */}
         <button
@@ -49,7 +51,7 @@ export default function MemberProfile({
       </div>
 
       {/* 멤버 */}
-      <div className="relative w-[450px]">
+      <div className="relative w-full">
         <div className="flex items-center justify-center gap-x-10 h-[120px] bg-gray-100 rounded-2xl shadow-[0_0_6px_rgba(0,0,0,0.1)]">
           {members && members.length > 0 ? (
             members.map((m) => (
--- src/routes/MyStudyRoom/MyStudyRoomPage.jsx
+++ src/routes/MyStudyRoom/MyStudyRoomPage.jsx
@@ -1,5 +1,4 @@
 import MemberProfile from "../../components/MyStudyRoom/MemberProfile";
-import StudyRoomRule from "../../components/MyStudyRoom/StudyRoomRule";
 import { IoChatbubbleEllipses } from "react-icons/io5";
 import StudyRoomApi from "../../api/studyRoomAPI";
 import { useNavigate, useParams } from "react-router-dom";
@@ -9,6 +8,8 @@ import StudyNote from "./StudyNote";
 import StudyChat from "./StudyChat";
 import StudyRecruitFormModal from "../../components/StudyRecruit/StudyRecruitFormModal";
 import StudyRecruitApi from "../../api/studyRecruitAPI";
+import StudyRoomRule from "../../components/MyStudyRoom/StudyroomRule";
+import MemberProfileSkeleton from "../../components/skeleton/MyStudyRoom/MemberProfileSkeleton";
 
 export default function MyStudyRoomPage() {
   const { studyRoomId } = useParams(); // 스터디방 id
@@ -19,6 +20,12 @@ export default function MyStudyRoomPage() {
   const [recruitLoading, setRecruitLoading] = useState(false);
   const [showRecruitModal, setShowRecruitModal] = useState(false);
   const navigate = useNavigate();
+  const [showSkeleton, setShowSkeleton] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSkeleton(false), 2000);
+    return () => clearTimeout(Timer);
+  }, []);
 
   useEffect(() => {
     setLoading(true);
@@ -67,19 +74,24 @@ export default function MyStudyRoomPage() {
           {studyRoom.name}
         </h1>
       </div>
-      <div className="rounded-2xl shadow-[0_0_6px_rgba(0,0,0,0.1)] p-4">
+      <div className="w-full max-w-[1000px] rounded-2xl shadow-[0_0_6px_rgba(0,0,0,0.1)] p-4">
         <div className="flex flex-col justify-center mx-auto gap-4 w-full">
-          <div className="flex justify-center mx-auto gap-4">
-            <MemberProfile
-              leader={leader}
-              members={members}
-              studyRecruit={studyRecruit}
-              recruitLoading={recruitLoading}
-              onRecruitWrite={() => setShowRecruitModal(true)}
-            />
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start gap-4 w-full">
+            {showSkeleton ? (
+              <MemberProfileSkeleton />
+            ) : (
+              <MemberProfile
+                leader={leader}
+                members={members}
+                studyRecruit={studyRecruit}
+                recruitLoading={recruitLoading}
+                onRecruitWrite={() => setShowRecruitModal(true)}
+              />
+            )}
+
             <StudyRoomRule rule={studyRoom.rule} />
           </div>
-          <div className="flex justify-center font-bold w-full rounded-2xl shadow-[0_0_6px_rgba(0,0,0,0.1)] p-4 mx-auto">
+          <div className="flex justify-center font-bold w-full max-w-[1000px] rounded-2xl shadow-[0_0_6px_rgba(0,0,0,0.1)] p-4 mx-auto">
             🔔 {studyRoom.notice || "오늘의 공지는 없습니다!"}
           </div>
         </div>
