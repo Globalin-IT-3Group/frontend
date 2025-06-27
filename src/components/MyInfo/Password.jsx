847aca36c714aeaf73f2c422e5172b62ab6dbf37
agreenroom <tjwlals7805@gmail.com>
2025-06-27 19:50:37 +0900

    fix: 내정보페이지 비밀번호 변경시 사라짐 수정

--- src/components/MyInfo/Password.jsx
+++ src/components/MyInfo/Password.jsx
@@ -57,8 +57,8 @@ export default function Password({ initPassword }) {
       await userAPI.updatePassword(password);
       setPasswordMatchMessage("비밀번호가 성공적으로 변경되었습니다!");
       setIsPasswordMatch(true);
-      setPassword("");
-      setConfirmPassword("");
+      // setPassword("");
+      // setConfirmPassword("");
     } catch (e) {
       setPasswordMatchMessage(
         e?.response?.data?.message || "비밀번호 변경에 실패했습니다."
